8801978ddf86c7e1df239d3f421c62f60e2de3bc
Paulino Auyon <pauyon@jgboswell.com>
2023-01-24 16:37:07 -0800

    Update gitignore Add User Model Add login page

---
+++ BlazorShopDemo/BlazorShopDemo/Shared/User.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorShopDemo.Shared
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
