07c95df6b85885284fd66d82fa69a1307d6e48b3
Paulino Auyon <pauyon@jgboswell.com>
2023-05-13 09:07:15 -0700

    Add mudblazor to blazor server app

--- BlazorShopDemo/BlazorShopDemo.BlazorServer/Program.cs
+++ BlazorShopDemo/BlazorShopDemo.BlazorServer/Program.cs
@@ -6,12 +6,15 @@ using BlazorShopDemo.BlazorServer.Services.CategoryService;
 using BlazorShopDemo.BlazorServer.Services.ProductService;
 using BlazorShopDemo.BlazorServer.Services.StatsService;
 using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7059/") });
 
